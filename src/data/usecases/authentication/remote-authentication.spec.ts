5f48a4b8a908255bd66a53526e4f1191c71e0bc6
Tiago Matos <tiagovolta@gmail.com>
2021-08-14 12:31:00 -0300

    fix: remote-authentication adjust

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -4,7 +4,7 @@ import { RemoteAuthentication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
   test('Should call HttpPostClient with correct URL', async () => {
-    class HttpPostClientSpy implements HasdttpPostClient {
+    class HttpPostClientSpy implements HttpPostClient {
       url?: string
       async post (url: string): Promise<void> {
         this.url = url

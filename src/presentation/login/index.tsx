ed285a7e4d09e5eaf4b47dc57a70ec63fbee44f5
Tiago Matos <tiagovolta@gmail.com>
2021-08-19 21:30:02 -0300

    refactor: created input component

--- src/presentation/login/index.tsx
+++ src/presentation/login/index.tsx
@@ -3,6 +3,7 @@ import Styles from './login-styles.scss'
 import Spinner from '@/presentation/components/spinner/spinner'
 import LoginHeader from '@/presentation/components/login-header/login-header'
 import Footer from '@/presentation/components/footer/footer'
+import Input from '@/presentation/components/input/input'
 
 const Login: React.FC = () => {
   return (
@@ -10,8 +11,8 @@ const Login: React.FC = () => {
       <LoginHeader />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <input type="email" name="email" placeholder="Digite seu e-mail" />
-        <input type="password" name="password" placeholder="Digite sua senha" />
+        <Input type="text" name="email" placeholder="Digite seu e-mail" />
+        <Input type="password" name="password" placeholder="Digite sua senha" />
         <button type="submit">Entrar</button>
         <span className={Styles.link}>Criar conta</span>
         <div className={Styles.erroWrap}>

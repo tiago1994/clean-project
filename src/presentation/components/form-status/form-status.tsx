9932d32417a43f72dc3342354fc4af57ac4c698b
Tiago Matos <tiagovolta@gmail.com>
2021-08-19 23:54:16 -0300

    feat: created context for error and small changes

--- src/presentation/components/form-status/form-status.tsx
+++ src/presentation/components/form-status/form-status.tsx
@@ -4,11 +4,11 @@ import { Spinner } from '@/presentation/components'
 import Context from '@/presentation/contexts/form/form-context'
 
 const FormStatus: React.FC = () => {
-  const { isLoading, errorMessage } = useContext(Context)
+  const { state, errorState } = useContext(Context)
   return (
     <div data-testid="error-wrap" className={Styles.erroWrap}>
-      { isLoading && <Spinner className={Styles.spinner} /> }
-      { errorMessage && <span className={Styles.error}>Erro</span> }
+      { state.isLoading && <Spinner className={Styles.spinner} /> }
+      { errorState.main && <span className={Styles.error}>{errorState.main}</span> }
     </div>
   )
 }

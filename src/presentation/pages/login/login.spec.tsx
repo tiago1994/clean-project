1792b30d89c97a221cd5e123d56315fe05bfc0df
Tiago Matos <tiagovolta@gmail.com>
2021-08-21 16:01:23 -0300

    test: created new test in email input

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,24 +1,50 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
+import { Validation } from '@/presentation/protocols/validation'
+import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
+import faker from 'faker'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+  validade (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Login />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Login validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('Login Component', () => {
+  afterEach(cleanup)
+
   test('Should start with initial state', () => {
     const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+  })
+
+  test('Should call Validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const emailInput = sut.getByTestId('email')
+    const fakeEmail = faker.internet.email()
+    fireEvent.input(emailInput, { target: { value: fakeEmail } })
+    expect(validationSpy.input).toEqual({
+      email: fakeEmail
+    })
   })
 })

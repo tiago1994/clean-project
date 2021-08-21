585a8c22faf8f504f91a808482dd3b742833c3bd
Tiago Matos <tiagovolta@gmail.com>
2021-08-21 16:06:08 -0300

    test: created new password input field

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -38,13 +38,23 @@ describe('Login Component', () => {
     expect(submitButton.disabled).toBe(true)
   })
 
-  test('Should call Validation with correct value', () => {
+  test('Should call Validation with correct email', () => {
     const { sut, validationSpy } = makeSut()
     const emailInput = sut.getByTestId('email')
     const fakeEmail = faker.internet.email()
     fireEvent.input(emailInput, { target: { value: fakeEmail } })
     expect(validationSpy.input).toEqual({
       email: fakeEmail
+    })
+  })
+
+  test('Should call Validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    const fakePassword = faker.internet.password()
+    fireEvent.input(passwordInput, { target: { value: fakePassword } })
+    expect(validationSpy.input).toEqual({
+      password: fakePassword
     })
   })
 })

7bae79fca80bb58a8372d0752e7903d282810bfc
Tiago Matos <tiagovolta@gmail.com>
2021-08-22 11:13:35 -0300

    feat: prevent double submit in form login

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -111,4 +111,11 @@ describe('Login Component', () => {
       password
     })
   })
+
+  test('Should call authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+    expect(authenticationSpy.callsCount).toBe(1)
+  })
 })

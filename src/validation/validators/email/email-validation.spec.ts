5b3c33190175669d874b8570a897c88f6d211d88
Tiago Matos <tiagovolta@gmail.com>
2021-08-22 23:39:55 -0300

    refactor: created makeSut for EmailValidation

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,16 +2,18 @@ import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
 
+const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+
 describe('EmailValidation', () => {
   test('Should return error if emails is invalid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const sut = makeSut()
     const falsyEmail = faker.random.words()
     const error = sut.validate(falsyEmail)
     expect(error).toEqual(new InvalidFieldError(falsyEmail))
   })
 
   test('Should return falsy if emails is valid', () => {
-    const sut = new EmailValidation(faker.random.word())
+    const sut = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })

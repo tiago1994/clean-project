5e428c21860e4db41515f738e2bf2776180f9a59
Tiago Matos <tiagovolta@gmail.com>
2021-08-22 23:20:54 -0300

    feat: created factory for requiredfieldvalidation

--- src/validation/required-field/required-field-validation.ts
+++ src/validation/required-field/required-field-validation.ts
@@ -3,6 +3,7 @@ import { RequiredFieldError } from '@/validation/errors'
 
 export class RequiredFieldValidation implements FieldValidation {
   constructor (readonly field: string) {}
+
   validate (value: string): Error {
     return value ? null : new RequiredFieldError()
   }

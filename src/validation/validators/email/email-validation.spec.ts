8fc1139904c3e33e3f363ba29d739b8a28e94caf
Tiago Matos <tiagovolta@gmail.com>
2021-08-25 22:58:48 -0300

    feat: add Login composition

--- src/validation/validators/email/email-validation.spec.ts
+++ src/validation/validators/email/email-validation.spec.ts
@@ -2,14 +2,14 @@ import faker from 'faker'
 import { InvalidFieldError } from '@/validation/errors'
 import { EmailValidation } from './email-validation'
 
-const makeSut = (): EmailValidation => new EmailValidation(faker.database.column())
+const makeSut = (): EmailValidation => new EmailValidation('email')
 
 describe('EmailValidation', () => {
   test('Should return error if emails is invalid', () => {
     const sut = makeSut()
     const falsyEmail = faker.random.words()
     const error = sut.validate(falsyEmail)
-    expect(error).toEqual(new InvalidFieldError(falsyEmail))
+    expect(error).toEqual(new InvalidFieldError('email'))
   })
 
   test('Should return falsy if emails is valid', () => {
--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -1,5 +1,5 @@
 import faker from 'faker'
-import { FieldValidationSpy } from '@/validation/validators/test'
+import { FieldValidationSpy } from '@/validation/test'
 import { ValidationComposite } from './validation-composite'
 
 type SutTypes = {

427f6e715ef37fedd07d7c935355d3da60172da9
Tiago Matos <tiagovolta@gmail.com>
2021-09-04 14:46:08 -0300

    feat: ensure CompareFieldValidation returns falsy if validation succeeds

---
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -0,0 +1,21 @@
+import { CompareFieldsValidation } from './compare-fields-validation'
+import { InvalidFieldError } from '@/validation/errors'
+import faker from 'faker'
+
+const makeSut = (valueToCompare: string): CompareFieldsValidation => new CompareFieldsValidation(faker.database.column(), valueToCompare)
+
+describe('CompareFieldsValidation', () => {
+  test('Should return error if compare is invalid', () => {
+    const valueToCompare = faker.random.word()
+    const sut = makeSut(faker.random.word())
+    const error = sut.validate(valueToCompare)
+    expect(error).toEqual(new InvalidFieldError(valueToCompare))
+  })
+
+  test('Should return falsy if compare is valid', () => {
+    const valueToCompare = faker.random.word()
+    const sut = makeSut(valueToCompare)
+    const error = sut.validate(valueToCompare)
+    expect(error).toBeFalsy()
+  })
+})
---
+++ src/validation/validators/compare-fields/compare-fields-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from '@/validation/protocols'
+import { InvalidFieldError } from '@/validation/errors'
+
+export class CompareFieldsValidation implements FieldValidation {
+  constructor (readonly field: string, private readonly valueToCompare: string) {}
+
+  validate (value: string): Error {
+    return value !== this.valueToCompare ? new InvalidFieldError(value) : null
+  }
+}

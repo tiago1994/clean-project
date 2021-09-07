3d5b88da8179ca3db81e325cea394cb11c76d11e
Tiago Matos <tiagovolta@gmail.com>
2021-09-07 12:17:51 -0300

    test: ensure login begin with correct initial value

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,11 @@
 describe('Login', () => {
-  it('Shuld load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+  it('Shuld load with correct initial state', () => {
+    cy.get('[data-testid="email-status"]').should('have.attr', 'title', 'Campo obrigatório')
+    cy.get('[data-testid="password-status"]').should('have.attr', 'title', 'Campo obrigatório')
+    cy.get('[data-testid="submit"]').should('have.attr', 'disabled')
+    cy.get('[data-testid="error-wrap"]').should('not.have.descendants')
   })
 })

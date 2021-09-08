d902643f82fd2b230b20f64086530f89653f7468
Tiago Matos <tiagovolta@gmail.com>
2021-09-08 18:52:35 -0300

    feat: created error if accessToken is undefined

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -4,6 +4,7 @@ const baseUrl: string = Cypress.config().baseUrl
 
 describe('Login', () => {
   beforeEach(() => {
+    cy.server()
     cy.visit('login')
   })
 
@@ -24,25 +25,36 @@ describe('Login', () => {
   })
 
   it('Shuld present valid state if form is valid', () => {
+    cy.route({
+      method: 'POST',
+      url: /login/,
+      status: 401,
+      response: {
+        error: faker.random.words()
+      }
+    })
     cy.get('[data-testid="email"]').type(faker.internet.email())
     cy.get('[data-testid="password"]').type(faker.random.alphaNumeric(10))
     cy.get('[data-testid="submit"]').click()
-    cy.get('[data-testid="error-wrap"]')
-      .get('[data-testid="spinner"]').should('exist')
-      .get('[data-testid="main-error"]').should('not.exist')
-      .get('[data-testid="spinner"]').should('not.exist')
-      .get('[data-testid="main-error"]').should('contain.text', 'Credenciais inválidas')
+    cy.get('[data-testid="spinner"]').should('not.exist')
+    cy.get('[data-testid="main-error"]').should('contain.text', 'Credenciais inválidas')
     cy.url().should('eq', `${baseUrl}/login`)
   })
 
   it('Shuld save accessToken if valid credentials are provided', () => {
+    cy.route({
+      method: 'POST',
+      url: /login/,
+      status: 200,
+      response: {
+        accessToken: faker.datatype.uuid()
+      }
+    })
     cy.get('[data-testid="email"]').type('mango@gmail.com')
     cy.get('[data-testid="password"]').type('12345')
     cy.get('[data-testid="submit"]').click()
-    cy.get('[data-testid="error-wrap"]')
-      .get('[data-testid="spinner"]').should('exist')
-      .get('[data-testid="main-error"]').should('not.exist')
-      .get('[data-testid="spinner"]').should('not.exist')
+    cy.get('[data-testid="spinner"]').should('not.exist')
+    cy.get('[data-testid="main-error"]').should('not.exist')
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })

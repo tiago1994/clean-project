e1a1d8f7c2304ac498433d0626674a48e7d25338
Tiago Matos <tiagovolta@gmail.com>
2021-09-13 20:25:00 -0300

    feat: created PrivateRoute to check if accessToken exists in LocalStorage

--- src/main/routes/router.tsx
+++ src/main/routes/router.tsx
@@ -2,21 +2,23 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { makeLogin } from '@/main/factories/pages/login/login-factory'
 import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
-import { setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
+import { getCurrentAccountAdapter, setCurrentAccountAdapter } from '@/main/adapters/current-account-adapter'
 import { SurveyList } from '@/presentation/pages'
 import { ApiContext } from '@/presentation/contexts'
+import { PrivateRoute } from '@/presentation/components'
 
 const Router: React.FC = () => {
   return (
     <ApiContext.Provider
       value={{
-        setCurrentAccount: setCurrentAccountAdapter
+        setCurrentAccount: setCurrentAccountAdapter,
+        getCurrentAccount: getCurrentAccountAdapter
       }}>
       <BrowserRouter>
         <Switch>
           <Route path="/login" component={makeLogin} exact />
           <Route path="/signup" component={makeSignUp} exact />
-          <Route path="/" component={SurveyList} exact />
+          <PrivateRoute path="/" component={SurveyList} exact />
         </Switch>
       </BrowserRouter>
     </ApiContext.Provider>
---
+++ src/presentation/components/private-route/private-route.spec.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import { createMemoryHistory, MemoryHistory } from 'history'
+import { Router } from 'react-router-dom'
+import PrivateRoute from './private-route'
+import { ApiContext } from '@/presentation/contexts'
+import { mockAccountModel } from '@/domain/test'
+
+type SutTypes = {
+  history: MemoryHistory
+}
+
+const makeSut = (account = mockAccountModel()): SutTypes => {
+  const history = createMemoryHistory({ initialEntries: ['/'] })
+  render(
+    <ApiContext.Provider value={{ getCurrentAccount: () => account }}>
+      <Router history={history}>
+        <PrivateRoute />
+      </Router>
+    </ApiContext.Provider>
+  )
+
+  return {
+    history
+  }
+}
+
+describe('PrivateRoute', () => {
+  test('Should redirect to /login if token is empty', () => {
+    const { history } = makeSut(null)
+    expect(history.location.pathname).toBe('/login')
+  })
+
+  test('Should render current component', () => {
+    const { history } = makeSut()
+    expect(history.location.pathname).toBe('/')
+  })
+})
